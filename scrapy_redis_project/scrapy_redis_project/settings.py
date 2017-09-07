a64f2402a2e41898d81bcc385f454129e3ff2d58
sixGodChan <sunqim16@gmail.com>
2017-09-07 22:55:30 +0800

    scrapy_redis

--- scrapy_redis_project/scrapy_redis_project/settings.py
+++ scrapy_redis_project/scrapy_redis_project/settings.py
@@ -98,19 +98,38 @@ ROBOTSTXT_OBEY = True
 REDIS_URL = 'redis://sunq:123@192.168.99.136:6379'  # 连接URL（优先于以上配置）
 # REDIS_PARAMS  = {}                                  # Redis连接参数             默认：REDIS_PARAMS = {'socket_timeout': 30,'socket_connect_timeout': 30,'retry_on_timeout': True,'encoding': REDIS_ENCODING,}）
 # REDIS_PARAMS['redis_cls'] = 'myproject.RedisClient' # 指定连接Redis的Python模块  默认：redis.StrictRedis
-REDIS_ENCODING = "utf-8"                            # redis编码类型             默认：'utf-8'
+REDIS_ENCODING = "utf-8"  # redis编码类型             默认：'utf-8'
 
 # ##### 去重规则 #####
 DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
 
 # ##### 使用调度器 #####
+# from scrapy_redis.scheduler import Scheduler
 SCHEDULER = "scrapy_redis.scheduler.Scheduler"
 
+# from scrapy_redis.queue import PriorityQueue
+# from scrapy_redis import picklecompat
+
 SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.PriorityQueue'  # 默认使用优先级队列（默认），其他：PriorityQueue（有序集合），FifoQueue（列表）、LifoQueue（列表）
-SCHEDULER_QUEUE_KEY = '%(spider)s:requests'  # 调度器中请求存放在redis中的key
+SCHEDULER_QUEUE_KEY = '%(spider)s:requests'  # 调度器中请求存放在redis中的key jandan:requests 有序set
 SCHEDULER_SERIALIZER = "scrapy_redis.picklecompat"  # 对保存到redis中的数据进行序列化，默认使用pickle
 SCHEDULER_PERSIST = True  # 是否在关闭时候保留原来的调度器和去重记录，True=保留，False=清空
 SCHEDULER_FLUSH_ON_START = True  # 是否在开始之前清空 调度器和去重记录，True=清空，False=不清空
 SCHEDULER_IDLE_BEFORE_CLOSE = 10  # 去调度器中获取数据时，如果为空，最多等待时间（最后没数据，未获取到）。
-SCHEDULER_DUPEFILTER_KEY = '%(spider)s:dupefilter'  # 去重规则，在redis中保存时对应的key
+SCHEDULER_DUPEFILTER_KEY = '%(spider)s:dupefilter'  # 去重规则，在redis中保存时对应的key jandan:dupefilter 字典
 SCHEDULER_DUPEFILTER_CLASS = 'scrapy_redis.dupefilter.RFPDupeFilter'  # 去重规则对应处理的类
+
+# ##### 数据持久化 #####
+from scrapy_redis.pipelines import RedisPipeline
+
+ITEM_PIPELINES = {
+    'scrapy_redis.pipelines.RedisPipeline': 300,
+}
+REDIS_ITEMS_KEY = '%(spider)s:items'  # 指定key jandan:items 列表
+REDIS_ITEMS_SERIALIZER = 'json.dumps'  # 序列化函数
+
+# ##### 起始url #####
+
+REDIS_START_URLS_KEY = '%(name)s:start_urls'  # jandan:start_urls 列表
+
+REDIS_START_URLS_AS_SET = False # 取列表取值，如果是True，向集合中取
--- scrapy_redis_project/scrapy_redis_project/spiders/jandan.py
+++ scrapy_redis_project/scrapy_redis_project/spiders/jandan.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
-import scrapy
+# import scrapy
+#
+#
+# class JandanSpider(scrapy.Spider):
+#     name = 'jandan'
+#     allowed_domains = ['jandan.com']
+#     start_urls = ['http://jandan.net/']
+#
+#     def parse(self, response):
+#         print(response)
+
+# 起始URL从redis的Key中获取
+
+from scrapy_redis.spiders import RedisSpider
 
 
-class JandanSpider(scrapy.Spider):
+class JandanSpider(RedisSpider):
     name = 'jandan'
     allowed_domains = ['jandan.com']
-    start_urls = ['http://jandan.net/']
+    start_urls = ['http://jandan.net/']  # 不用自己写url，自动去redis中拿
 
     def parse(self, response):
         print(response)
+
